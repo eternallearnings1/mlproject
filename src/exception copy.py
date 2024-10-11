bb96b242d592eb54be9535fb23cd6cd8c9f4af99
eternallearnings1 <eternallearnings1@gmail.com>
2024-10-11 13:22:38 -0700

    src files of data ingestion and transformations

---
+++ src/exception copy.py
@@ -0,0 +1,24 @@
+import sys
+from logger import logging  # Import the logger configuration from logger.py
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error))
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+# Simulating an exception
+try:
+    result = 1 / 0
+except Exception as e:
+    # Use CustomException and log the error in the same folder as the logger
+    logging.error(CustomException(e, sys))
